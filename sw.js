124e43316eb98b24c3ff2929fb68cc02c966c078
kohshi529-glitch <kohshi529@gmail.com>
2026-08-16 15:26:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'questboard-v4-3';
+const CACHE = 'questboard-v4-4';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon-180.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
